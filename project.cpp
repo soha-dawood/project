187b499fb52eee7b3a37c37bee28024d99c3af69
soha-dawood <sohadawood2003@gmail.com>
2024-09-08 18:15:57 +0300

    1&2

--- project.cpp
+++ project.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <windows.h>
+#include <string>
 using namespace std;
+void setColor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void clearScreen() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = {0, 0};
+    DWORD written;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    FillConsoleOutputCharacter(hConsole, ' ', csbi.dwSize.X * csbi.dwSize.Y, coord, &written);
+    SetConsoleCursorPosition(hConsole, coord);
+}
+
+void printCenteredText(const string &text, int width) {
+    int len = text.length();
+    int spaces = (width - len) / 2;
+    for (int i = 0; i < spaces; ++i) cout << " ";
+    cout << text;
+}
+
+void printWelcomeToSSHBank() {
+    cout <<"*                 *   * * * * *   *          *********   ***********  ***          ***  * * * * * "<<endl;
+    cout <<"*                 *   *           *          *           *         *  *  *        *  *  *         "<<endl;
+    cout <<"*       ***       *   *           *          *           *         *  *   *      *   *  *          "<<endl;
+    cout <<"*     *     *     *   *********   *          *           *         *  *    *    *    *  *********  "<<endl;
+    cout <<"*    *       *    *   *           *          *           *         *  *     *  *     *  *          "<<endl;
+    cout <<"*   *         *   *   *           *          *           *         *  *      **      *  *          "<<endl;
+    cout <<"*  *           *  *   *           *          *           *         *  *              *  *          "<<endl;
+    cout <<"***             ***   * * * * *   *********  **********  ***********  *              *  * * * * *   "<<endl;
+
+}
 class Person{
     int id;
     string name;
@@ -105,6 +140,28 @@ public:
 };
 int main ()
 {
+     // Set text color (Light Green for this example)
+  setColor(10);
+
+    // Get console dimensions
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  GetConsoleScreenBufferInfo(hConsole, &csbi);
+  int consoleWidth = csbi.dwSize.X;
+
+    // Print centered text
+  clearScreen(); // Clear screen before printing
+  printWelcomeToSSHBank();
+
+    // Wait for 5 seconds
+  Sleep(20000);
+
+  // Clear the screen before exiting
+  clearScreen();
+
+  // Reset color to default
+  setColor(7); // Default color
+
   Clint c(51,"sohadawood","8451316465",20000);
   Clint c1(6546,"ahmedmohamed","846511115",20000);
   c1.CheckBalance();
